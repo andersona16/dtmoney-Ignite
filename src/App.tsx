f240d9aa5cf5509dba82b1f6fdc12a554e5da814
Anderson <andersonaraujoc1@gmail.com>
2022-03-25 22:52:22 -0300

    Atualização
    Adicionado Assets (Imagens) para a aplicação

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 65px;
+`
+
+export function App() {
+  return (
+    <div className="App">
+      <Title >Hello World</Title>
+    </div>
+  );
+}
